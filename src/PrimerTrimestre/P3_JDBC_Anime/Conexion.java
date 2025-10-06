9fd533b886704e034eb7ef766626a2c21032a6dd
ManuelCarreraP <mcarrerapazo@danielcastelao.org>
2025-10-06 09:56:46 +0200

    Tarea 3 - Conexion a BD

---
+++ src/PrimerTrimestre/P3_JDBC_Anime/Conexion.java
@@ -0,0 +1,23 @@
+package PrimerTrimestre.P3_JDBC_Anime;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+    private static final String URL = "jdbc:postgresql://10.0.2.15:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String PASS = "admin";
+    public static Connection conexion() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USUARIO, PASS);
+        } catch (ClassNotFoundException e) {
+            System.out.println("La conexión es estable: " + e.getMessage());
+            return null;
+        } catch (SQLException e) {
+            System.out.println("No se pudo establecer conexión: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
